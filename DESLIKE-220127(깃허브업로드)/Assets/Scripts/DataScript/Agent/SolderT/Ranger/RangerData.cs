6ddd3d00b9e668209f476754e7a2722e1aeeff64
parknyeongjoon <idpasudwns@naver.com>
2022-03-02 19:49:31 +0900

    2022.03.02

--- DESLIKE-220127(깃허브업로드)/Assets/Scripts/DataScript/Agent/SolderT/Ranger/RangerData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SoldierData",menuName = "ScriptableObject/Agent/RangerData")]
-public class RangerData : SoldierData
-{
-
-}

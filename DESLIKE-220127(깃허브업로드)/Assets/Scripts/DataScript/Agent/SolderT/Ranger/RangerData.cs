315eb3320d62023a5684ca6fdf8a02f9789a85e5
parknyeongjoon <idpasudwns@naver.com>
2022-03-02 19:23:53 +0900

    Revert "2022.03.02"
    This reverts commit 4a6168752bd5230f7109c47c7856e2c3b6e6f507.

---
+++ DESLIKE-220127(깃허브업로드)/Assets/Scripts/DataScript/Agent/SolderT/Ranger/RangerData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoldierData",menuName = "ScriptableObject/Agent/RangerData")]
+public class RangerData : SoldierData
+{
+
+}

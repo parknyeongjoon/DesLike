0792b9a7f49edee119d555b5e8fb200cf8109834
parknyeongjoon <50522036+parknyeongjoon@users.noreply.github.com>
2022-04-02 00:21:27 +0900

    Delete DESLIKE-220127(깃허브업로드) - 복사본 directory

--- DESLIKE-220127(깃허브업로드) - 복사본/Assets/Scripts/DataScript/BasicAttack/BasicAttackData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BasicAttackData : ScriptableObject
-{
-    public float start_Delay, end_Delay, range, atk_Dmg;
-    public AttackArea atkArea;
-}
--- DESLIKE-220127(깃허브업로드) - 복사본/Assets/Scripts/DataScript/SkillT/ActiveSkill/ActiveSkillData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ActiveSkillData : SkillData
-{
-    public float cooltime, mp, range, start_Delay, end_Delay;
-    public AttackArea atkArea;
-}

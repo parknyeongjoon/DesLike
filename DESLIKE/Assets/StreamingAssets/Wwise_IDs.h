2a76548a71ada22269e326e35fa44c9eb0cbfc0a
yea2463 <yea2463@gmail.com>
2022-04-03 18:56:03 +0900

    작업
    배경재생
    소리제한
    Idle/Dead사운드 추가
    끗

--- DESLIKE/Assets/StreamingAssets/Wwise_IDs.h
+++ DESLIKE/Assets/StreamingAssets/Wwise_IDs.h
@@ -13,8 +13,15 @@ namespace AK
 {
     namespace EVENTS
     {
+<<<<<<< Updated upstream
         static const AkUniqueID DEAD_OR_ALIVE = 3575957003U;
         static const AkUniqueID SKELETON_ATK = 180084035U;
+=======
+        static const AkUniqueID BATTLE_END = 461545889U;
+        static const AkUniqueID BATTLE_START = 2706291346U;
+        static const AkUniqueID SKELETON_ATK = 180084035U;
+        static const AkUniqueID SKELETON_DEAD = 3710894717U;
+>>>>>>> Stashed changes
         static const AkUniqueID SKELETON_MOVE = 3933512188U;
         static const AkUniqueID TEST_UI = 1896376002U;
     } // namespace EVENTS
@@ -44,7 +51,10 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+<<<<<<< Updated upstream
         static const AkUniqueID SKELETON_ATK_BUS = 2491616377U;
+=======
+>>>>>>> Stashed changes
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES

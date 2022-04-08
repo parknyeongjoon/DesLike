8e43ef92b4a060ad3d72eac13d2a23cb202ae16d
parknyeongjoon <idpasudwns@naver.com>
2022-04-08 23:42:58 +0900

    ㅁㄴㅇㄹ

--- DESLIKE/Assets/Scripts/DataScript/Agent/Hero/HeroDie.cs
+++ DESLIKE/Assets/Scripts/DataScript/Agent/Hero/HeroDie.cs
@@ -11,7 +11,7 @@ public class HeroDie : MonoBehaviour
         Debug.Log("Game Over");
         //Destroy(gameObject);
         SaveManager.Instance.gameData.canContinue = false;
-        AkSoundEngine.PostEvent("23101_Die", gameObject);
+        AkSoundEngine.PostEvent("H_23101_Die", gameObject);
         GameManager.Instance.gamePause = true;
         Time.timeScale = 0;
         //SceneManager.LoadScene("MainTitle");//창 띄우고 거기서 버튼 클릭 시 이동하게 만들기

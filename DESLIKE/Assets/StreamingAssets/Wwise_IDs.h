09afc74fb43f759d7f58329e4ed1dfd29b974922
yea2463 <yea2463@gmail.com>
2022-05-02 18:57:59 +0900

    이벤트삭제 / 11303
    11303아이들Sfx 없는 상태
    H_23101 이벤트 안넘어오는 상태

--- DESLIKE/Assets/StreamingAssets/Wwise_IDs.h
+++ DESLIKE/Assets/StreamingAssets/Wwise_IDs.h
@@ -13,8 +13,6 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID BATTLE_END = 461545889U;
-        static const AkUniqueID BATTLE_START = 2706291346U;
         static const AkUniqueID GAME_SPEED_PAUSE = 1027347108U;
         static const AkUniqueID GAME_SPEED_RESUME = 2372597559U;
         static const AkUniqueID H_23101_DIE = 3025390842U;
@@ -24,19 +22,10 @@ namespace AK
         static const AkUniqueID MUSIC_MAP = 3017247325U;
         static const AkUniqueID MUSIC_S2_NORMAL_BATTLE = 382373357U;
         static const AkUniqueID MUSIC_START = 3725903807U;
-        static const AkUniqueID T_12202_ATK_1 = 450034178U;
-        static const AkUniqueID T_12202_IDLE = 3619100458U;
-        static const AkUniqueID T_12202_SKILL_1 = 1526852227U;
-        static const AkUniqueID T_12301_ATK_1 = 1080515304U;
-        static const AkUniqueID T_12301_IDLE = 880908724U;
-        static const AkUniqueID T_12302_ATK_1 = 2037815843U;
-        static const AkUniqueID T_12302_IDLE = 270789025U;
-        static const AkUniqueID T_12305_IDLE = 2413801456U;
-        static const AkUniqueID T_12305_SKILL_1 = 2724418929U;
-        static const AkUniqueID T_23101_ATK_1 = 3073476802U;
-        static const AkUniqueID T_23101_DIE = 617022462U;
-        static const AkUniqueID T_23101_IDLE = 1608577898U;
-        static const AkUniqueID T_23101_SKILL_1 = 2895012419U;
+        static const AkUniqueID N_11303_ATT = 1947476378U;
+        static const AkUniqueID N_11303_DIE = 1562429897U;
+        static const AkUniqueID N_11303_IDLE = 4143119307U;
+        static const AkUniqueID N_11303_SKILL_1 = 2196727988U;
     } // namespace EVENTS
 
     namespace STATES
@@ -86,11 +75,19 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID CHARACTER_BUS = 2026837964U;
+        static const AkUniqueID H_BUS = 1962540672U;
+        static const AkUniqueID H_DIE_BUS = 843762955U;
+        static const AkUniqueID H_IDLE_BUS = 1194134643U;
+        static const AkUniqueID H_SKILL_1 = 3269956185U;
+        static const AkUniqueID H_SKILL_2 = 3269956186U;
+        static const AkUniqueID H_SKILL_3 = 3269956187U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC = 3991942870U;
-        static const AkUniqueID TE_BUS = 986497999U;
-        static const AkUniqueID TN_BUS = 208977400U;
-        static const AkUniqueID TNE = 1097649576U;
+        static const AkUniqueID N_ATT = 2083319893U;
+        static const AkUniqueID N_BUS = 1798247302U;
+        static const AkUniqueID N_DIE = 2065703678U;
+        static const AkUniqueID N_IDLE = 1311555690U;
+        static const AkUniqueID N_SKILL_1 = 2870636867U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES

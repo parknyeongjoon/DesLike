95ae2089648daaad6b95f4d2631c34bb3b633245
parknyeongjoon <idpasudwns@naver.com>
2022-05-20 16:29:06 +0900

    relic 구조 수정 마무리

--- DESLIKE/Assets/Scripts/DataScript/Relic/RelicManager.cs
+++ DESLIKE/Assets/Scripts/DataScript/Relic/RelicManager.cs
@@ -34,12 +34,30 @@ public class RelicManager : MonoBehaviour
 
     public void GetRelic(string relicKey)//relicKey에 해당하는 유물을 흭득하는 함수
     {
-        if (SaveManager.Instance.dataSheet.relicDataSheet[relicKey])
+        if (SaveManager.Instance.dataSheet.relicDataSheet[relicKey])//데이터 시트에 있는 유물인지 검사
         {
             GameObject relicObject = SaveManager.Instance.dataSheet.relicObjectSheet[relicKey];
             relicList.Add(relicKey, relicObject.GetComponent<Relic>());
             Instantiate(relicObject, relicCanvas.transform.GetChild(0).transform);
             relicList[relicKey].DoEffect();
+        }
+        else
+        {
+            Debug.Log("유물 키 없음");
+        }
+    }
+
+    public void LoadRelic(string relicKey)
+    {
+        if (SaveManager.Instance.dataSheet.relicDataSheet[relicKey])//데이터 시트에 있는 유물인지 검사
+        {
+            GameObject relicObject = SaveManager.Instance.dataSheet.relicObjectSheet[relicKey];
+            relicList.Add(relicKey, relicObject.GetComponent<Relic>());
+            Instantiate(relicObject, relicCanvas.transform.GetChild(0).transform);
+            if (relicList[relicKey].relicData.continueReuse)//로드하면 다시 사용해야하는 유물이라면 재사용
+            {
+                relicList[relicKey].DoEffect();
+            }
         }
         else
         {
